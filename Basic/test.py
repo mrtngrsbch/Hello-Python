026fa4339bd59ab160564596f6d605eb9f156921
Martin Gersbach <martin@gersbach.net>
2025-07-24 07:30:26 -0300

    new

---
+++ Basic/test.py
@@ -0,0 +1,15 @@
+class Perro:
+    def hablar(self):
+        print("¡Guau, Guau!")
+
+class Gato:
+    def hablar(self):
+        print("¡Miau, Miau!")
+
+class Vaca:
+    def hablar(self):
+        print("¡Muuu, Muuu!")
+
+lista = [Perro(), Gato(), Vaca()]
+for animal in lista:
+    animal.hablar()
